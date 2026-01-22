c1591cd54b292b26b035d8b6e2a2b731f49e868b
Mohammad Sheraj <mdsheraj123@gmail.com>
2026-01-22 23:16:24 +0530

    Update

--- CMake/testing/cpCtest.cpp
+++ CMake/testing/cpCtest.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <fstream>
 #include <string>
 
@@ -6,29 +7,31 @@ using namespace std;
 int main() {
     ifstream expectedOutput("../testing/expectedOutput.txt");
     ifstream output("../output.txt");
-
+    
     int result = 0;
-
+    
     while (true) {
         string line1, line2;
         getline(expectedOutput, line1);
         getline(output, line2);
-
+        
         if (line1 != line2) {
+            cout << line1 << " not same as " << line2 << endl;
             result = 1;
             break;
         }
-
+        
         if (!output && !expectedOutput) {
             break;
         } else if (!output || !expectedOutput) {
+            cout<< "Different number of lines." << endl;
             result = 1;
             break;
         }
     }
-
+    
     output.close();
     expectedOutput.close();
-
+    
     return result;
 }
